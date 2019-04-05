5b53212f564e6c5f947b9808f415aa7e126d7305
Simone Amadio <amadiosimone1996@gmail.com>
2019-04-05 15:59:25 +0200

    notes

--- Lab/Day2/loop_schedule.c
+++ Lab/Day2/loop_schedule.c
@@ -18,8 +18,17 @@ void print_usage( int * a, int N, int nthreads ) {
 }
 
 int main( int argc, char * argv[] ) {
+  const int N=250;
+  int a[N];
+  int thread_id=0;
+  int nthreads=1;
 
-  // TODO                                                                                                                                                                                                                                                                                
-
+  for(int i = 0; i < N; i++)
+  {
+    a[i]=thread_id;
+  }
+  
+  print_usage(a,N,nthreads);
+  printf("hello\n");
   return 0;
 }
